721b23240dfb47da0ab5d1abaff884e77e24d365
vishal singh rathore <vishalsinghrathore@vishals-MacBook-Air.local>
2025-04-17 21:47:23 +0530

    first commit

---
+++ config/initializers/cors.rb
@@ -0,0 +1,7 @@
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins '*'  # Allow all origins (for development)
+      resource '*', headers: :any, methods: [:get, :post, :patch, :delete, :options]
+    end
+  end
+  
